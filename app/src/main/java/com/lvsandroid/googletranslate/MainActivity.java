de6c7de7029cf840dafbd22dc73bff3d5a2f0278
Laurent <you@example.com>
2016-12-14 20:56:10 +0700

    Initial commit

---
+++ app/src/main/java/com/lvsandroid/googletranslate/MainActivity.java
@@ -0,0 +1,51 @@
+package com.lvsandroid.googletranslate;
+
+import android.os.AsyncTask;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+
+import com.google.cloud.translate.Translate;
+import com.google.cloud.translate.TranslateOptions;
+import com.google.cloud.translate.Translation;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    // todo API_KEY should not be stored in plain sight
+    private static String apiKey;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        final TextView textView = (TextView) findViewById(R.id.text_view);
+        final Handler textViewHandler = new Handler();
+
+        apiKey =  getResources().getString(R.string.API_KEY);
+
+        new AsyncTask<Void, Void, Void>() {
+            @Override
+            protected Void doInBackground(Void... params) {
+                TranslateOptions options = TranslateOptions.newBuilder()
+                        .setApiKey(apiKey)
+                        .build();
+                Translate translate = options.getService();
+                final Translation translation =
+                        translate.translate("Hello World",
+                                Translate.TranslateOption.targetLanguage("de"));
+                textViewHandler.post(new Runnable() {
+                    @Override
+                    public void run() {
+                        if (textView != null) {
+                            textView.setText(translation.getTranslatedText());
+                        }
+                    }
+                });
+                return null;
+            }
+        }.execute();
+    }
+}
